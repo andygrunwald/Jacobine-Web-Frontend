e6ca98cd429328c25ee4d76e5d70d3baa2fce64b
Andy Grunwald <andygrunwald@gmail.com>
2013-09-10 19:43:43 +0200

    Cleaned up copyright typoscript, creates a twitter link and added a version number

--- www/typo3conf/ext/analysis/Configuration/TypoScript/Page.Default/copyright.ts
+++ www/typo3conf/ext/analysis/Configuration/TypoScript/Page.Default/copyright.ts
@@ -1,16 +1,19 @@
 analysis.copyright = COA
 analysis.copyright {
     10 = TEXT
-    10.value = &copy; Andy Grunwald /&nbsp;
+    10.value = &copy; Andy Grunwald
 
     20 = TEXT
     20.value = @andygrunwald
-    20.value.typolink.extTarget = https://twitter.com/andygrunwald
+    20.typolink.parameter = https://twitter.com/andygrunwald
+    20.typolink.title = Andy Grunwald on twitter
+    20.noTrimWrap = | (|) |
 
     30 = TEXT
-    30.value = &nbsp;
+    30.data = date:U
+    30.strftime = %Y
 
     40 = TEXT
-    40.data = date:U
-    40.strftime = %Y
+    40.value = v0.0.1 alpha
+    40.noTrimWrap = | / | |
 }

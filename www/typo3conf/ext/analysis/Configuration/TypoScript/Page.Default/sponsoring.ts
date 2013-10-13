d90279fdcdb7acdf3d585cd4b1109af5e9fb4769
Andy Grunwald <andygrunwald@gmail.com>
2013-10-13 14:06:54 +0200

    Added Pixolith logo, because they sponsor this server :))))

---
+++ www/typo3conf/ext/analysis/Configuration/TypoScript/Page.Default/sponsoring.ts
@@ -0,0 +1,14 @@
+analysis.sponsoring = COA
+analysis.sponsoring {
+    10 = IMAGE
+    10.file = {$path.full.img}logo-pixolith.jpg
+    10.file.height = 50
+
+    10.titleText = Server sponsored by pixolith
+    10.imageLinkWrap = 1
+    10.imageLinkWrap {
+        enable = 1
+        typolink.parameter = http://www.pixolith.de/
+        typolink.title = Pixolith GbR
+    }
+}
